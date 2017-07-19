aaf010edeb3647362675ee90f50e8d5606578070
Keegan Campbell <Keegan.Campbell@alaskaair.com>
2017-07-18 17:52:34 -0700

    Initial commit.

---
+++ FlightSearch/FlightSearch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlightSearch.Startup))]
+namespace FlightSearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

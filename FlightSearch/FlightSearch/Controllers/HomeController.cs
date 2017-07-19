fcb69fd287cdf12869af59a8912f416ca76c3696
Keegan Campbell <Keegan.Campbell@alaskaair.com>
2017-07-19 14:58:22 -0700

    Communication between client/server side established.

--- FlightSearch/FlightSearch/Controllers/HomeController.cs
+++ FlightSearch/FlightSearch/Controllers/HomeController.cs
@@ -9,16 +9,25 @@ using FlightSearch.ViewModels;
 
 namespace FlightSearch.Controllers
 {
+    enum SortBy
+    {
+        MainCabinLowToHigh,
+        MainCabinHighToLow,
+        FirstClassLowToHigh,
+        FirstClassHighToLow,
+        DepartureEarlyToLate,
+        DepartureLateToEarly
+    }
     public class HomeController : Controller
     {
+        private readonly CsvLoader _csvHelper = new CsvLoader();
+
         public ActionResult Index(HomeViewModel homeViewModel)
         {
-            Helpers.CsvLoader csvHelper = new Helpers.CsvLoader();
-            var airports = csvHelper.GetAirports();
-            var flights = csvHelper.GetFlights();
+            var airports = _csvHelper.GetAirports();
+            var flights = _csvHelper.GetFlights();
 
-            // add another dropdown for other airport.
-            // add a list to display filtered flights.
+            // add a table to display filtered flights.
             homeViewModel.Airports = GetSelectListItemsAirports(airports);
             homeViewModel.Flights = flights;
             return View(homeViewModel);
@@ -30,6 +39,21 @@ namespace FlightSearch.Controllers
                 Value = element.Code,
                 Text = element.Name
             }).ToList();
+        }
+
+        public ActionResult GetFilteredFlights(string departureAirportCode, string arrivalAirportCode, int sortBy)
+        {
+            SortBy sortingOrder = (SortBy)sortBy;
+            var initialFlights = _csvHelper.GetFlights();
+            var filteredFlights = SortingHelper.FilterFlightsByAirportPair(departureAirportCode, arrivalAirportCode, initialFlights);
+
+            // TODO:
+            // move enum class to separate file/folder
+            // add switch statement here for sorting
+            // in view, add onchange to each dropdown (including sort order dropdown)
+            // then populate table based on results of json
+
+            return Json(new { success = true, flights = filteredFlights });
         }
 
         public ActionResult About()
---
+++ FlightSearch/FlightSearch/Scripts/Flights.js
@@ -0,0 +1,32 @@
+﻿function getFilteredFlights() {
+    console.log("entered get filtered flights function")
+    jQuery.ajax({
+        url: requestUrl,
+        type: 'POST',
+        data: { departureAirportCode: document.getElementById("ddlDepartureAirports").value, arrivalAirportCode: document.getElementById("ddlArrivalAirports").value, sortBy: document.getElementById("sortingDropdown").value },
+        async: false,
+        success: function(response) {
+            if (response != null && response.success) {
+                console.log("Got flight data!");
+                console.log(response);
+                /*var availableSlots;
+                availableSlots = '<option>Pick available time</option>';
+                for (var i = 0; i < response.timeslots.length; i++) {
+                    availableSlots += '<option>' + response.timeslots[i] + '</option>';
+                }
+                $("#ddlTimeSlots").html(availableSlots).selectpicker('refresh');    */
+            } else {
+                /*var availableSlots;
+                availableSlots = '<option>No available times</option>';
+                $("#ddlTimeSlots").html(availableSlots).selectpicker('refresh');    */
+            }
+        },
+        error: function(data) {
+            console.log("Failed to get flight data.");
+            /*var availableSlots = '<option>No available times</option>';
+            $("#ddlTimeSlots").html(availableSlots).selectpicker('refresh');*/
+        }
+    });
+}
+
+getFilteredFlights();
